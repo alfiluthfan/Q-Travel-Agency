f2cbacc98e2599ceb1aea90004bc89784f92c443
alfiluthfan <alfiluthfan26@gmail.com>
2024-05-08 10:17:34 +0700

    Last commit

--- js/script.js
+++ js/script.js
@@ -11,3 +11,24 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
 };
+
+
+
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    var email = document.getElementById("emailInput").value;
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    
+    if (!emailRegex.test(email)) {
+        document.getElementById("errorText").style.display = "block";
+        event.preventDefault(); // Menghentikan perilaku default formulir
+    } else {
+        document.getElementById("errorText").style.display = "none";
+        // Tampilkan popup berhasil setelah submit berhasil
+        document.getElementById("successPopup").style.display = "block";
+        // Beri waktu delay sebelum menghilangkan popup
+        setTimeout(function() {
+            document.getElementById("successPopup").style.display = "none";
+        }, 3000); // Delay 3000ms (3 detik)
+        event.preventDefault(); // Menghentikan perilaku default formulir
+    }
+});
